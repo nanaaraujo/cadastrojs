67e820c5a6d1a77afbeaf76c11eb50218f6aeaa4
nanaaraujo <euivanalais@gmail.com>
2019-01-13 20:22:04 -0300

    add gitignore, package. Reformulou o programa.

--- index.js
+++ index.js
@@ -1,50 +1,73 @@
+var prompt = require('prompt');
+
+prompt.start();
+
+
+
 const bancodeDados = {
     admin: "Michel",
-   usuario: { name: "Michel", username: "micheldiz", senha: "12345678" }
+   usuario: { name: "Michel", username: "micheldiz", senha: "123" }
 };
-// console.log(bancodeDados)  
+
 console.log(bancodeDados)
 
-let login = prompt("Deseja fazer um cadastro?")
-console.log(login) 
+var property = {
+   // configuraçoes padroes do questionario de cadastro 
+   name: 'yesno',
+   message: 'Deseja fazer um cadastro?',
+   validator: /y[es]*|n[o]?/,
+   warning: 'Must respond yes or no'
+ };
 
-if (login === true) { 
- //Cadastro
-  let usuario = prompt("Seu nome de usuário")  
-  let senha = prompt("Sua senha")
-  
- if (usuario == ' ') {
-alert("Nome do usuário incompleto")
-} else {
- bancodeDados.usuario.user = usuario
- bancodeDados.usuario.username = usuario
- bancodeDados.usuario.senha = senha
-}
-   } 
 
-else {
- //Login 
+const startq = () => { 
+ // função de Cadastro
+  let usuario = () => prompt.get(['User'], function (err, result) {
+     // pede um novo nome para cadastro
+   bancodeDados.usuario.user = result.User
+   bancodeDados.usuario.username = result.User
+   senha()
+ });
+  let senha = () => prompt.get(['Senha'], function (err, result) {
+     // pede uma nova senha para cadastro
+   bancodeDados.usuario.senha = result.Senha
+   console.log(bancodeDados)
+ });
 
-let resposta = prompt("Seu nome de usuário") 
-
-const checarSenha = () => {
- let senhaUser = prompt("Digite Sua senha")
-  if (senhaUser == bancodeDados.usuario.senha) {
-alert("Senha está correta")
-} else {
- alert("Senha Incorreta")
-}
-}
-
-if (resposta == bancodeDados.usuario.username) {
-alert("Nome do usuário correto")
- console.log(bancodeDados)
- checarSenha()
-} else {
- alert("Usuário não existe")
-}
+ usuario() // chama a função usuário para iniciar o processo de cadastro
 
    }
 
+   const iniciarlogin = () => {
+      // função de Login
+      let usuario = () => prompt.get(['User'], function (err, result) {
+         // pede o nome de usuario e verifica no db se existe
+         if (bancodeDados.usuario.username == result.User){
+            passosenha()
+         } else {
+            console.log('usuário não existe')
+         }
+      });
+      const passosenha = () => prompt.get(['Senha'], function (err, result) {
+         // pede a senha e verifica se está correta
+         if (bancodeDados.usuario.senha == result.Senha){
+            console.log('Senha correta')
+         } else {
+            console.log('Senha incorreta')
+         }
+        
+      });
+   
+      
+      usuario()
+      }
 
-
+      // questiona se o usuário quer fazer cadastro
+   prompt.get(property, function (err, result) {
+      console.log(result,"result login")
+      if (result.yesno == 'yes') {
+         startq()
+      } else  {
+         iniciarlogin()
+      }
+    });
